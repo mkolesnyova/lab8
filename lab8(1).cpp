2177880be48374a6d9d2330453a09bfb7f4fc7ba
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-12-13 01:13:12 +0300

    Add files via upload

---
+++ lab8(1).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int n, i, d;
+	char str[20];
+	cout << " n= ";
+	cin >> n;
+	cout << " name : ";
+	cin >> str;
+	d = strlen(str);
+	cout << "d =" << d << endl;
+	str[d] = '\0';
+	for (i = 0; i < n; i++)
+		cout << str << endl;
+	return(0);
+}
---
+++ lab8(2).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int k=0, i;
+	const int n = 20;
+	char str[n];
+	char a ;
+	a = 'и';
+	cout << "Введите предложение : ";
+	for (int i = 0; i < n-1; i++)
+	{
+		cin >> str[i];
+		if ((str[i] == a) && ((i + 1) % 2 != 1) && (i > 0)) k++;
+	}
+	str[n-1] = '\0';
+	cout << "Вы ввели:" << str << endl;
+	for (i = 0; i < k; i++)
+		cout << a << endl;
+	return 0;
+}
+
+
+
+
