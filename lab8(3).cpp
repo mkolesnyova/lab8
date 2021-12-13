cfea2d6cadacb4892ff1ab655b2231529463e0eb
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-12-13 09:24:47 +0300

    Update lab8(3).cpp

--- lab8(3).cpp
+++ lab8(3).cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main()
 {
-
+	const int d = 10;
+	char s[d];
+	char i;
+	int n, m;
+	cout << " vvedite slovo : ";
+	for (i = 0; i < d - 1; i++)
+		cin >> s[i];
+	s[d - 1] = '\0';
+	cout << " m= ";
+	cin >> m;
+	cout << " n= ";
+	cin >> n;
+	i = s[m];
+	s[m] = s[n];
+	s[n] = i;
+	cout << " novoe slovo :" << s;
 
 }
