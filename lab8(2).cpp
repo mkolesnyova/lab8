4d2009dfb040d1b1c3a7e55637b6b07499fcfff7
mkolesnyova <92516020+mkolesnyova@users.noreply.github.com>
2021-12-13 09:03:58 +0300

    Update lab8(2).cpp

--- lab8(2).cpp
+++ lab8(2).cpp
@@ -1,25 +1,21 @@
-﻿#include <iostream>
-#include <string>
+#include <iostream>
 using namespace std;
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	int k=0, i;
+	int k = 0, i;
 	const int n = 20;
 	char str[n];
-	char a ;
-	a = 'и';
-	cout << "Введите предложение : ";
-	for (int i = 0; i < n-1; i++)
+	char a('y');
+	cout << " vvedite predlozhenye : ";
+	for (int i = 0; i < n - 1; i++)
 	{
 		cin >> str[i];
 		if ((str[i] == a) && ((i + 1) % 2 != 1) && (i > 0)) k++;
 	}
-	str[n-1] = '\0';
-	cout << "Вы ввели:" << str << endl;
+	str[n - 1] = '\0';
+	cout << "vy vveli :" << str << endl;
 	for (i = 0; i < k; i++)
 		cout << a << endl;
-	return 0;
 }
 
 
